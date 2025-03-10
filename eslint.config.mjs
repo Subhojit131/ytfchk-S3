42ef5bb1f10e46e44f3926422bbb3fab50df260b
Subhojit131 <subhojitmandal147@gmail.com>
2025-03-11 00:52:02 +0530

    Add Prettier and ESLint config files

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import pluginPrettier from "eslint-plugin-prettier";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    files: ["src/**/*.js", "src/**/*.mjs"],
+  },
+  pluginJs.configs.recommended,
+  {
+    plugins: { prettier: pluginPrettier },
+    rules: {
+      "no-unused-vars": "warn",
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+      "eqeqeq": "error",
+      "prettier/prettier": "error", // Enforce Prettier as an ESLint rule
+    },
+  },
+  prettier, // Disables ESLint rules that conflict with Prettier
+];
+
